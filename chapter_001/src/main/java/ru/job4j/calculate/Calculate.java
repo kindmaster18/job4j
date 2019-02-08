16c7f110db7320767a8e8d8adfcc5080f4745b93
Dmitry Amelin <kindmaster18@yandex.ru>
2019-02-08 21:13:04 +0300

    Добавлено javadoc для Calculate и main и файл package-info.java

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,16 @@
 package ru.job4j.calculate;
-public class Calculate
-{
-    public static void main(String[] args)
-  {
+
+/**
+ *Class Calculate решение задачи части 001 урок 1.
+ *@author Dmitry Amelin
+ *@since 07.02.2019
+ */
+  public class Calculate {
+	  /**
+	   *Main.
+	   *@param args - args.
+	   */
+     public static void main(String[] args) {
 	      System.out.println("Hello World");
+     }
   }
-}
