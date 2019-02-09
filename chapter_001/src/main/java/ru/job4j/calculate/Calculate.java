25de2e4db0ec4b20cbf3cba830bebcf0277ff217
Dmitry Amelin <kindmaster18@yandex.ru>
2019-02-09 03:57:10 +0300

    Поправил ошибки инспектора

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,7 +9,7 @@ package ru.job4j.calculate;
  */
 public class Calculate {
 
-	/**
+    /**
      * Main.
      * @param args - args.
      */
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,23 +5,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Dmitry Amelin (kindmaster18@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Dmitry Amelin (kindmaster18@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenThreeEchoPlusName() {
-    String input = "Dmitry Amelin";
-    String expect = "Echo, echo, echo : Dmitry Amelin"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Dmitry Amelin";
+        String expect = "Echo, echo, echo : Dmitry Amelin";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
